5794bb8ff09dec81d25a8c9f8715031271cfcf7b
sergey kalinin <core.editor>
2014-10-30 22:08:13 +0400

    modals for news tape compl

--- blogik5/static/js/helper/helper.js
+++ blogik5/static/js/helper/helper.js
@@ -1,4 +1,20 @@
 (function (){
-	
+	// -------------------------------------------------------------------------------------- windows
+	$('.all_news .more_link').on('click', function(event){	
+		event.preventDefault();
+
+		console.log(1111);
+
+		var	text = $(this).closest('.article').find('.info').html(),
+			head = $(this).closest('.article').find('.h3').html();
+
+		console.log(head);
+		console.log(text);
+
+		$('#modal-9 .modal-body').html(text);
+		$('#modal-9 .modal-title').html(head);
+
+		$('#modal-9').modal();
+	});	
 })();
 
